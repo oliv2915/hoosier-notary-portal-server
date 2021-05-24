217623e26606f034ff72176a291b267902b597c0
Brian Oliver II <brian.d.oliver@icloud.com>
2021-05-24 16:46:39 -0400

    initial deploy

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
